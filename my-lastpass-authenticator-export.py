1ac9a4657613d85baca897445779202ac058155d
Paolo Barolat-Romana <pbarolat@hotmail.com>
2023-01-14 13:42:46 -0500

    Changed exit command

--- my-lastpass-authenticator-export.py
+++ my-lastpass-authenticator-export.py
@@ -159,13 +159,8 @@ def main():
         totp = pyotp.parse_uri(totpinput)
     except Exception as error:
         print("The OTP URL entered is incorrect", error)
-        exit
-    # Below are the attributes for the otp object
-    #['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'at', 'byte_secret', 'digest', 'digits', 'generate_otp', 'int_to_bytestring', 'interval', 'issuer', 'name', 'now', 'provisioning_uri', 'secret', 'timecode', 'verify']
+        os._exit()
 
-
-        exit
- 
     write_out(totp, totpinput, destdir)
 
 
